f4530489f32a6d8fac73e8f5b72cd345cd461f2e
John Serrano <johnserrano404@gmail.com>
2016-05-11 14:08:29 -0700

    remove libshell dependencies

---
+++ utils/read_line.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "utils.h"
+
+char *read_line(int fd) {
+    int maximumLineLength = 128;
+    char *lineBuffer = malloc(sizeof(char) * maximumLineLength);
+    char ch;
+    char *line;
+    int count = 0;
+
+    read(fd, &ch, 1);
+
+    if (lineBuffer == NULL) {
+      exit(1);
+    }
+
+    while (ch != '\n') {
+        if (count == maximumLineLength) {
+            maximumLineLength += 128;
+            lineBuffer = realloc(lineBuffer, maximumLineLength);
+            if (lineBuffer == NULL) {
+                exit(1);
+            }
+        }
+        lineBuffer[count] = ch;
+        count++;
+
+        read(fd, &ch, 1);
+    }
+
+    lineBuffer[count] = '\0';
+    line = malloc(sizeof(char) * (count + 1));
+    string_copy(line, lineBuffer);
+    free(lineBuffer);
+    return line;
+}
--- utils/utils.h
+++ utils/utils.h
@@ -3,3 +3,6 @@ int string_to_int(char *s);
 int len_command(char **command);
 int strings_compare(char *s1, const char *s2);
 void cmd_exit(char **command);
+char *string_copy(char *dest, char *src);
+char *read_line(int fd);
+char **string_split(char *str, char seperator);
