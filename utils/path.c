29f5db6f6aa56fe80e00ddf6ae1e3758f926f616
John Serrano <johnserrano404@gmail.com>
2016-05-13 15:33:49 -0700

    cleaned up path. surprised it worked before

--- utils/path.c
+++ utils/path.c
@@ -22,11 +22,14 @@ void *ret_correct_path(char *cmd, __attribute__((unused)) char **env) {
   if (check_path(cmd_cpy) == 1) {
     /* check if path exists */
     i = open(cmd_cpy, O_RDONLY);
-    if (i < 0) return NULL;
+    if (i < 0) {
+      free(cmd_cpy);
+      return NULL;
+    }
     close(i);
     return cmd_cpy;
   }
-  
+
   str = get_env("PATH");
   path = string_split(str, ':');
 
@@ -34,27 +37,24 @@ void *ret_correct_path(char *cmd, __attribute__((unused)) char **env) {
   for (i=0; path[i] != 0; i++) {
     /* -> open path find file return cmd after str cat*/
     pDir = opendir(path[i]);
-    if (pDir == NULL) {
-      continue;
-    }
-    else {
-      while ((pDirent = readdir(pDir)) != NULL) {
-        if (strings_compare(cmd, pDirent->d_name) == 0) {
-          free(cmd_cpy);
-          cmd_cpy = malloc(sizeof(char)*(str_len(path[i]) + str_len(cmd) + 2));
-          cmd_cpy = string_copy(cmd_cpy, path[i]);
-          str_cat(cmd_cpy, "/");
-          str_cat(cmd_cpy, cmd);
-          free_command(path);
-          free(str);
-          closedir(pDir);
-          return cmd_cpy;
-        }
+
+    while ((pDirent = readdir(pDir)) != NULL) {
+      if (strings_compare(cmd, pDirent->d_name) == 0) {
+        free(cmd_cpy);
+        cmd_cpy = malloc(sizeof(char)*(str_len(path[i]) + str_len(cmd) + 2));
+        cmd_cpy = string_copy(cmd_cpy, path[i]);
+        str_cat(cmd_cpy, "/");
+        str_cat(cmd_cpy, cmd);
+        free_command(path);
+        free(str);
+        closedir(pDir);
+        return cmd_cpy;
       }
     }
     closedir(pDir);
   }
   free_command(path);
+  free(cmd_cpy);
   /* Command not found, return null */
   return NULL;
 }
