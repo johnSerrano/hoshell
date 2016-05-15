9f0b59f97f55e92d4f0cfb2ecbbb2a3a8e40101f
butcallmejo <13@holbertonschool.com>
2016-05-15 16:50:47 -0700

    c style coding on hoshell.c

--- hoshell.c
+++ hoshell.c
@@ -7,97 +7,96 @@
 void fork_exec(char **command, char **env);
 int check_builtins(char **command, __attribute__((unused)) char **env);
 
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, char **env) {
-  char **command;
-  char *line;
-
-  /* moves environment variables to the heap */
-  init_env();
-
-  /* read from stdin */
-  while (1) {
-    write(1, "HOS :: ", 7);
-    line = read_line(0);
-
-    /*ignore comments*/
-    command = string_split(line, '#');
-    if (command[0] == NULL) {
-      free_command(command);
-      free(line);
-      continue;
-    };
-    string_copy(line, command[0]);
-    free_command(command);
-
-    command = string_split(line, ' ');
-    if (command[0] == NULL) {
-      free_command(command);
-      free(line);
-      continue;
-    };
-
-    fork_exec(command, env);
-    free_command(command);
-    free(line);
-  }
-}
-
-/* run a command without killing the main thread */
-void fork_exec(char **command, char **env) {
-  int status;
-  int pid;
-  char *cmd = command[0];
-
-  if (check_builtins(command, env)) return;
-  cmd = ret_correct_path(cmd, env);
-
-  if (cmd == NULL) {
-    write(2, command[0], str_len(command[0]));
-    write(2, ": Command not found\n", 20);
-    return;
-  }
-
-  pid = fork();
-  if (pid == -1) {
-    write(2, "Fork failed", 11);
-  } else if (pid == 0) {
-    execve(cmd, command, env);
-  } else {
-    wait(&status);
-    update_status(status);
-  }
-  free(cmd);
-  return;
-}
-
-/* check builtin commands before searching path.
- * returns 1 if found, 0 otherwise
+/*
+ * Highly Original SHELL
  */
-int check_builtins(char **command, char **env) {
-  if (strings_compare(command[0], "exit") == 0) {
-    cmd_exit(command);
-    /* shouldn't run except if there is an error exiting */
-    return 1;
-  }
-  if (strings_compare(command[0], "env") == 0) {
-    print_env(env);
-    return 1;
-  }
-  if (strings_compare(command[0], "cd") == 0) {
-    if (len_command(command) <= 1) {
-      /* error */
-      return 1;
-    }
-    cd(command[1]);
-    return 1;
-  }
-  if (strings_compare(command[0], "pwd") == 0) {
-    pwd();
-    return 1;
-  }
-  if (strings_compare(command[0], "$?") == 0) {
-    print_status();
-    return 1;
-  }
-  return 0;
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, char **env)
+{
+	char **command;
+	char *line;
+	init_env(); /* moves environment variables to the heap */
+	/* read from stdin */
+	while (1) {
+		write(1, "HOS :: ", 7);
+		line = read_line(0);
+		/*ignore comments*/
+		command = string_split(line, '#');
+		if (command[0] == NULL) {
+			free_command(command);
+			free(line);
+			continue;
+		};
+		string_copy(line, command[0]);
+		free_command(command);
+		command = string_split(line, ' ');
+		if (command[0] == NULL) {
+			free_command(command);
+			free(line);
+			continue;
+		};
+		fork_exec(command, env);
+		free_command(command);
+		free(line);
+	}
+}
+
+/*
+ * Function to run a command without killing the main thread
+ */
+void fork_exec(char **command, char **env)
+{
+	int status;
+	int pid;
+	char *cmd = command[0];
+	if (check_builtins(command, env)) 	/*runs custom built cmds*/
+		return;
+	cmd = ret_correct_path(cmd, env); 	/*returns the correct cmd*/
+	if (cmd == NULL) { 			/*error checking*/
+		write(2, command[0], str_len(command[0]));
+		write(2, ": Command not found\n", 20);
+		return;
+	}
+	pid = fork();				/*starting child process*/
+	if (pid == -1) {			/*error checking*/
+		write(2, "Fork failed", 11);
+	} else if (pid == 0) {
+		execve(cmd, command, env);	/*executing correct cmd*/
+	} else {				/*TBD*/
+		wait(&status);
+		update_status(status);
+	}
+	free(cmd);
+	return;
+}
+
+/*
+ * Function to check builtin commands before searching path.
+ * returns 1 if found, 0 otherwise.
+ */
+int check_builtins(char **command, char **env)
+{
+	if (strings_compare(command[0], "exit") == 0) {
+		cmd_exit(command);
+		/* shouldn't run except if there is an error exiting */
+		return 1;
+	}
+	if (strings_compare(command[0], "env") == 0) {
+		print_env(env);
+		return 1;
+	}
+	if (strings_compare(command[0], "cd") == 0) {
+		if (len_command(command) <= 1)		/* error */
+			return 1;
+		cd(command[1]);
+		return 1;
+	}
+	if (strings_compare(command[0], "pwd") == 0) {
+		pwd();
+		return 1;
+	}
+	if (strings_compare(command[0], "$?") == 0) {
+		print_status();
+		return 1;
+	}
+	return 0;
 }
